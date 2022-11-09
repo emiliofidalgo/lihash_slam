39b24f4534db404affee86f7b984d3175f28945c
Mussol MAV <mussol@uib.es>
2022-11-09 12:17:29 +0100

    Fixing bugs in reconstruction

--- scripts/reconstruct.sh
+++ scripts/reconstruct.sh
@@ -17,9 +17,9 @@ if [ ! -d "$PCD_DIR" ]; then
     mkdir -p $PCD_DIR
     # Extracting point clouds from the bag file
     echo "Extracting point clouds from the bag file ..."
-    rosrun pcl_ros bag_to_pcd $BAG_DIR/*.bag /os_cloud/points $PCD_DIR
+    rosrun pcl_ros bag_to_pcd $BAG_DIR/pc*.bag /os_cloud/points $PCD_DIR
 fi
 
 # Calling the reconstruction script
 echo "Performing the reconstruction ..."
-roslauch lihash_slam reconstruct.launch input_dir:=$CURRENT_DIR
+roslaunch lihash_slam lihash_slam_reconstruct.launch input_dir:=$CURRENT_DIR
