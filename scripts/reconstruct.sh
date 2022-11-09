51058f7a8efb219c0345c90ec6ee8d23422595da
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2022-11-09 11:19:25 +0100

    Fixing the reconstruction script

--- scripts/reconstruct.sh
+++ scripts/reconstruct.sh
@@ -4,17 +4,20 @@
 
 # Variables
 CURRENT_DIR=$1
+BAG_DIR=$2
 PCD_DIR="$CURRENT_DIR/pcds/"
 
 # Starting reconstruction
-echo "Starting reconstruction from: $CURRENT_DIR"
+echo "Starting reconstruction"
+echo "SLAM Directory: $CURRENT_DIR"
+echo "Bag Directory: $BAG_DIR"
 
 # Check if the pcds directory exists
 if [ ! -d "$PCD_DIR" ]; then
     mkdir -p $PCD_DIR
     # Extracting point clouds from the bag file
     echo "Extracting point clouds from the bag file ..."
-    rosrun pcl_ros bag_to_pcd *.bag /os_cloud/points $PCD_DIR
+    rosrun pcl_ros bag_to_pcd $BAG_DIR/*.bag /os_cloud/points $PCD_DIR
 fi
 
 # Calling the reconstruction script
